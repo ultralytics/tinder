c52a59f3044a478f72b323f657f4fe8d96159ca5
Ultralytics Assistant <135830346+UltralyticsAssistant@users.noreply.github.com>
2024-09-06 00:16:28 +0800

    Merge Ultralytics Code Refactor
    Automated merge of Ultralytics Code Refactor

--- features.py
+++ features.py
@@ -1,5 +1,3 @@
-# coding=utf-8
-
 import random
 from datetime import date, datetime
 from time import sleep
@@ -45,7 +43,7 @@ def get_match_info():
             message = template.format(type(ex).__name__, ex.args)
             print(message)
             # continue
-    print(" %g matches... Done." % n)
+    print(f" {n:g} matches... Done.")
     return match_info
 
 
@@ -149,7 +147,7 @@ def pause():
     suspicious!
     """
     nap_length = 3 * random.random()
-    print("Napping for %f seconds..." % nap_length)
+    print(f"Napping for {nap_length:f} seconds...")
     sleep(nap_length)
 
 
